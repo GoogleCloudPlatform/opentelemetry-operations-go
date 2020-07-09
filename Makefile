f654a0bd3eaa92fa2ebb3604cd4693929644b186
James Bebbington <jbebbington@google.com>
2020-07-09 16:06:46 +1000

    Add lint config (#64)

--- Makefile
+++ Makefile
@@ -97,8 +97,8 @@ lint: $(TOOLS_DIR)/golangci-lint $(TOOLS_DIR)/misspell
 	set -e; for dir in $(ALL_GO_MOD_DIRS); do \
 	  echo "golangci-lint in $${dir}"; \
 	  (cd "$${dir}" && \
-	    $(TOOLS_DIR)/golangci-lint run --fix && \
-	    $(TOOLS_DIR)/golangci-lint run); \
+	    $(TOOLS_DIR)/golangci-lint --config $(CURDIR)/golangci.yml run --fix && \
+	    $(TOOLS_DIR)/golangci-lint --config $(CURDIR)/golangci.yml run); \
 	done
 	$(TOOLS_DIR)/misspell -w $(ALL_DOCS)
 	set -e; for dir in $(ALL_GO_MOD_DIRS) $(TOOLS_MOD_DIR); do \
--- example/trace/http/client/client.go
+++ example/trace/http/client/client.go
@@ -25,13 +25,14 @@ import (
 
 	"google.golang.org/grpc/codes"
 
-	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
 	"go.opentelemetry.io/otel/api/correlation"
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/kv"
 	"go.opentelemetry.io/otel/api/trace"
 	"go.opentelemetry.io/otel/plugin/httptrace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+
+	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
 )
 
 func initTracer() func() {
@@ -40,7 +41,7 @@ func initTracer() func() {
 	// Create Google Cloud Trace exporter to be able to retrieve
 	// the collected spans.
 	_, flush, err := texporter.InstallNewPipeline(
-		[]texporter.Option {texporter.WithProjectID(projectID)},
+		[]texporter.Option{texporter.WithProjectID(projectID)},
 		// For this example code we use sdktrace.AlwaysSample sampler to sample all traces.
 		// In a production application, use sdktrace.ProbabilitySampler with a desired probability.
 		sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
