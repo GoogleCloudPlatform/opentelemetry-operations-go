2f79f86b865fc4ddec3fc6dd32579baf5e1ad5b0
Punya Biswal <punya@google.com>
2021-03-08 18:57:14 +0000

    Prepare for releasing v0.17.0

--- exporter/metric/version.go
+++ exporter/metric/version.go
@@ -17,5 +17,5 @@ package metric
 // Version is the current release version of the OpenTelemetry
 // Operations Metric Exporter in use.
 func Version() string {
-	return "0.16.0"
+	return "0.17.0"
 }
