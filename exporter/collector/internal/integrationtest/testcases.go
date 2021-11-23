9d0431697f9a066f797a9d99ca4f3355ac8a888f
Aaron Abbott <aaronabbott@google.com>
2021-11-23 16:52:57 -0500

    Add delta temporality counter test fixture (#229)

--- exporter/collector/internal/integrationtest/testcases.go
+++ exporter/collector/internal/integrationtest/testcases.go
@@ -22,6 +22,11 @@ var (
 			ExpectFixturePath:    "testdata/fixtures/basic_counter_metrics_expect.json",
 		},
 		{
+			Name:                 "Delta Counter",
+			OTLPInputFixturePath: "testdata/fixtures/delta_counter_metrics.json",
+			ExpectFixturePath:    "testdata/fixtures/delta_counter_metrics_expect.json",
+		},
+		{
 			Name:                 "Non-monotonic Counter",
 			OTLPInputFixturePath: "testdata/fixtures/nonmonotonic_counter_metrics.json",
 			ExpectFixturePath:    "testdata/fixtures/nonmonotonic_counter_metrics_expect.json",
