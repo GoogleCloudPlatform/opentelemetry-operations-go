6f2d35b2a289e4d0845bcc6a3138684150ca3bd0
David Ashpole <dashpole@google.com>
2022-02-02 14:54:43 -0500

    Revert "merge col-exporter-rewrite into main (#295)"
    This reverts commit 7338096274174aa35c8249d0af6d5067565bddec.

--- exporter/collector/googlecloud_test.go
+++ exporter/collector/googlecloud_test.go
@@ -26,8 +26,11 @@ import (
 	internaldata "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/opencensus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
 	"go.opentelemetry.io/collector/model/pdata"
-	"go.uber.org/zap"
 	"google.golang.org/api/option"
 	cloudmetricpb "google.golang.org/genproto/googleapis/api/metric"
 	cloudtracepb "google.golang.org/genproto/googleapis/devtools/cloudtrace/v2"
@@ -57,17 +60,30 @@ func (ts *testServer) CreateSpan(context.Context, *cloudtracepb.Span) (*cloudtra
 func TestGoogleCloudTraceExport(t *testing.T) {
 	type testCase struct {
 		name        string
-		cfg         Config
+		cfg         *Config
 		expectedErr string
 	}
 
 	testCases := []testCase{
 		{
 			name: "Standard",
-			cfg: Config{
-				ProjectID:   "idk",
-				Endpoint:    "127.0.0.1:8080",
-				UseInsecure: true,
+			cfg: &Config{
+				ExporterSettings: config.NewExporterSettings(config.NewComponentID(typeStr)),
+				ProjectID:        "idk",
+				Endpoint:         "127.0.0.1:8080",
+				UseInsecure:      true,
+			},
+		},
+		{
+			name: "Standard_WithoutSendingQueue",
+			cfg: &Config{
+				ExporterSettings: config.NewExporterSettings(config.NewComponentID(typeStr)),
+				ProjectID:        "idk",
+				Endpoint:         "127.0.0.1:8080",
+				UseInsecure:      true,
+				QueueSettings: exporterhelper.QueueSettings{
+					Enabled: false,
+				},
 			},
 		},
 	}
@@ -83,7 +99,8 @@ func TestGoogleCloudTraceExport(t *testing.T) {
 			defer lis.Close()
 
 			go srv.Serve(lis)
-			sde, err := NewGoogleCloudTracesExporter(test.cfg, "latest", DefaultTimeout)
+
+			sde, err := newGoogleCloudTracesExporter(test.cfg, componenttest.NewNopExporterCreateSettings())
 			if test.expectedErr != "" {
 				assert.EqualError(t, err, test.expectedErr)
 				return
@@ -102,7 +119,7 @@ func TestGoogleCloudTraceExport(t *testing.T) {
 			span := ispans.Spans().AppendEmpty()
 			span.SetName(spanName)
 			span.SetStartTimestamp(pdata.NewTimestampFromTime(testTime))
-			err = sde.PushTraces(context.Background(), traces)
+			err = sde.ConsumeTraces(context.Background(), traces)
 			assert.NoError(t, err)
 
 			r := <-reqCh
@@ -162,31 +179,38 @@ func TestGoogleCloudMetricExport(t *testing.T) {
 
 	go srv.Serve(lis)
 
-	config := DefaultConfig()
-	config.ProjectID = "idk"
-	config.Endpoint = "127.0.0.1:8080"
-	config.UserAgent = "MyAgent {{version}}"
-	config.UseInsecure = true
-	config.MetricConfig.InstrumentationLibraryLabels = false
-	config.GetClientOptions = func() []option.ClientOption {
-		// Example with overridden client options
-		return []option.ClientOption{
-			option.WithoutAuthentication(),
-			option.WithTelemetryDisabled(),
-		}
+	// Example with overridden client options
+	clientOptions := []option.ClientOption{
+		option.WithoutAuthentication(),
+		option.WithTelemetryDisabled(),
 	}
 
-	sde, err := NewGoogleCloudMetricsExporter(context.Background(), config, zap.NewNop(), "v0.0.1", DefaultTimeout)
+	creationParams := componenttest.NewNopExporterCreateSettings()
+	creationParams.BuildInfo = component.BuildInfo{
+		Version: "v0.0.1",
+	}
+
+	sde, err := newGoogleCloudMetricsExporter(&Config{
+		ExporterSettings: config.NewExporterSettings(config.NewComponentID(typeStr)),
+		ProjectID:        "idk",
+		Endpoint:         "127.0.0.1:8080",
+		UserAgent:        "MyAgent {{version}}",
+		UseInsecure:      true,
+		GetClientOptions: func() []option.ClientOption {
+			return clientOptions
+		},
+	}, creationParams)
 	require.NoError(t, err)
 	defer func() { require.NoError(t, sde.Shutdown(context.Background())) }()
 
 	md := agentmetricspb.ExportMetricsServiceRequest{
 		Resource: &resourcepb.Resource{
+			Type: "host",
 			Labels: map[string]string{
-				"cloud.platform":          "gcp_kubernetes_engine",
 				"cloud.availability_zone": "us-central1",
-				"k8s.node.name":           "foo",
+				"host.name":               "foo",
 				"k8s.cluster.name":        "test",
+				"contrib.opencensus.io/exporter/stackdriver/project_id": "1234567",
 			},
 		},
 		Metrics: []*metricspb.Metric{
@@ -228,31 +252,39 @@ func TestGoogleCloudMetricExport(t *testing.T) {
 		},
 	}
 	md.Metrics[2].Resource = &resourcepb.Resource{
+		Type: "host",
 		Labels: map[string]string{
-			"cloud.platform":          "gcp_kubernetes_engine",
 			"cloud.availability_zone": "us-central1",
-			"k8s.node.name":           "bar",
+			"host.name":               "bar",
 			"k8s.cluster.name":        "test",
+			"contrib.opencensus.io/exporter/stackdriver/project_id": "1234567",
 		},
 	}
-	md.Metrics[3].Resource = &resourcepb.Resource{}
-	md.Metrics[4].Resource = &resourcepb.Resource{}
+	md.Metrics[3].Resource = &resourcepb.Resource{
+		Type: "host",
+		Labels: map[string]string{
+			"contrib.opencensus.io/exporter/stackdriver/project_id": "1234567",
+		},
+	}
+	md.Metrics[4].Resource = &resourcepb.Resource{
+		Type: "test",
+	}
 
-	assert.NoError(t, sde.PushMetrics(context.Background(), internaldata.OCToMetrics(md.Node, md.Resource, md.Metrics)), err)
+	assert.NoError(t, sde.ConsumeMetrics(context.Background(), internaldata.OCToMetrics(md.Node, md.Resource, md.Metrics)), err)
 
 	expectedNames := map[string]struct{}{
-		"workload.googleapis.com/test_gauge1": {},
-		"workload.googleapis.com/test_gauge2": {},
-		"workload.googleapis.com/test_gauge3": {},
-		"workload.googleapis.com/test_gauge4": {},
-		"workload.googleapis.com/test_gauge5": {},
+		"projects/idk/metricDescriptors/custom.googleapis.com/opencensus/test_gauge1": {},
+		"projects/idk/metricDescriptors/custom.googleapis.com/opencensus/test_gauge2": {},
+		"projects/idk/metricDescriptors/custom.googleapis.com/opencensus/test_gauge3": {},
+		"projects/idk/metricDescriptors/custom.googleapis.com/opencensus/test_gauge4": {},
+		"projects/idk/metricDescriptors/custom.googleapis.com/opencensus/test_gauge5": {},
 	}
 	for i := 0; i < 5; i++ {
 		drm := <-descriptorReqCh
 		assert.Regexp(t, "MyAgent v0\\.0\\.1", drm.metadata["user-agent"])
 		dr := drm.req.(*cloudmonitoringpb.CreateMetricDescriptorRequest)
-		assert.Contains(t, expectedNames, dr.MetricDescriptor.Type)
-		delete(expectedNames, dr.MetricDescriptor.Type)
+		assert.Contains(t, expectedNames, dr.MetricDescriptor.Name)
+		delete(expectedNames, dr.MetricDescriptor.Name)
 	}
 
 	trm := <-timeSeriesReqCh
@@ -264,20 +296,18 @@ func TestGoogleCloudMetricExport(t *testing.T) {
 		"node_name":    "foo",
 		"cluster_name": "test",
 		"location":     "us-central1",
+		"project_id":   "1234567",
 	}
 
 	resourceBar := map[string]string{
 		"node_name":    "bar",
 		"cluster_name": "test",
 		"location":     "us-central1",
+		"project_id":   "1234567",
 	}
 
-	// We no longer map to global resource by default.
-	// Instead, we map to generic_node
-	resourceDefault := map[string]string{
-		"location":  "global",
-		"namespace": "",
-		"node_id":   "",
+	resourceProjectID := map[string]string{
+		"project_id": "1234567",
 	}
 
 	expectedTimeSeries := map[string]struct {
@@ -285,30 +315,30 @@ func TestGoogleCloudMetricExport(t *testing.T) {
 		labels         map[string]string
 		resourceLabels map[string]string
 	}{
-		"workload.googleapis.com/test_gauge1": {
+		"custom.googleapis.com/opencensus/test_gauge1": {
 			value:          float64(1),
 			labels:         map[string]string{"k0": "v0"},
 			resourceLabels: resourceFoo,
 		},
-		"workload.googleapis.com/test_gauge2": {
+		"custom.googleapis.com/opencensus/test_gauge2": {
 			value:          float64(12),
 			labels:         map[string]string{"k0": "v0", "k1": "v1"},
 			resourceLabels: resourceFoo,
 		},
-		"workload.googleapis.com/test_gauge3": {
+		"custom.googleapis.com/opencensus/test_gauge3": {
 			value:          float64(123),
 			labels:         map[string]string{"k0": "v0", "k1": "v1", "k2": "v2"},
 			resourceLabels: resourceBar,
 		},
-		"workload.googleapis.com/test_gauge4": {
+		"custom.googleapis.com/opencensus/test_gauge4": {
 			value:          float64(1234),
 			labels:         map[string]string{"k0": "v0", "k1": "v1", "k2": "v2", "k3": "v3"},
-			resourceLabels: resourceDefault,
+			resourceLabels: resourceProjectID,
 		},
-		"workload.googleapis.com/test_gauge5": {
+		"custom.googleapis.com/opencensus/test_gauge5": {
 			value:          float64(34),
 			labels:         map[string]string{"k4": "v4", "k5": "v5"},
-			resourceLabels: resourceDefault,
+			resourceLabels: nil,
 		},
 	}
 	for i := 0; i < 5; i++ {
--- exporter/collector/integrationtests.md
+++ exporter/collector/integrationtests.md
@@ -6,7 +6,7 @@ and compare requests against recorded expectation fixtures. Currently, these tes
 metrics.
 
 The fixtures are located in the [`testdata/fixtures`][fixtures] directory and registered in
-[`integrationtest/testcases.go`][testcases].
+[`internal/integrationtest/testcases.go`][testcases].
 
 ## Running Tests
 
@@ -81,17 +81,16 @@ To add a new test:
 
     This is how the Ops Agent fixtures were generated.
 
-1. Add an entry in [`integrationtest/testcases.go`][testcases].
+1. Add an entry in [`internal/integrationtest/testcases.go`][testcases].
 1. Run the script to record the expectation fixture. This will contain the expected requests
     that the exporter makes to GCP services:
 
     ```sh
-    cd integrationtest
-    go run cmd/recordfixtures/main.go
+    go run internal/integrationtest/cmd/recordfixtures/main.go
     ```
 
     The generated file is a JSON encoded
-    [`MetricExpectFixture`](integrationtest/fixtures.proto#L21) protobuf message.
+    [`MetricExpectFixture`](internal/integrationtest/fixtures.proto#L21) protobuf message.
 
 See [#229](https://github.com/GoogleCloudPlatform/opentelemetry-operations-go/pull/229) for an
 example PR.
@@ -107,4 +106,4 @@ If you need to see the build logs because tests are failing, just ask someone to
 with you.
 
 [fixtures]: testdata/fixtures
-[testcases]: integrationtest/testcases.go
+[testcases]: internal/integrationtest/testcases.go
