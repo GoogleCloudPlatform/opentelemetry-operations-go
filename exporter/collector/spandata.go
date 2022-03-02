a4f95dbc502084f3eb9ea96e5d2fd2b27dd8dbe3
David Ashpole <dashpole@google.com>
2022-03-02 13:40:04 -0500

    rename files to be easier to understand, and add comment (#327)

--- exporter/collector/spandata.go
+++ exporter/collector/spandata.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package spandata defines translators from Trace proto spans to OpenCensus Go spanData.
 package collector
 
 import (
