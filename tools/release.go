04502016283d6e20392b55efcd855fbd7c2e6030
David Ashpole <dashpole@google.com>
2022-06-15 15:22:07 +0000

    prepare for v1.8.2 and v0.32.2

--- tools/release.go
+++ tools/release.go
@@ -29,8 +29,8 @@ import (
 const (
 	prefix = "github.com/GoogleCloudPlatform/opentelemetry-operations-go"
 
-	stable   = "1.8.1"
-	unstable = "0.32.1"
+	stable   = "1.8.2"
+	unstable = "0.32.2"
 )
 
 var versions = map[string]string{
