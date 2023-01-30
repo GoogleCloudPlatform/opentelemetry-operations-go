03219e63df29bc5d406256a24760d3baee7677d9
Mike Dame <mikedame@google.com>
2023-01-30 17:05:49 +0000

    Update versions to 1.11.0 and 0.35.0

--- tools/release.go
+++ tools/release.go
@@ -29,8 +29,8 @@ import (
 const (
 	prefix = "github.com/GoogleCloudPlatform/opentelemetry-operations-go"
 
-	stable   = "1.10.2"
-	unstable = "0.34.2"
+	stable   = "1.11.0"
+	unstable = "0.35.0"
 )
 
 var versions = map[string]string{
