2d501be40a7692d8eff376cbba2e7c6bd9a882d0
Pranav Sharma <pranav.ps95@hotmail.com>
2023-06-30 12:31:07 -0400

    Fix documentation for numberDataPointToValue
    Co-authored-by: Aaron Abbott <aaronabbott@google.com>

--- exporter/collector/metrics.go
+++ exporter/collector/metrics.go
@@ -943,7 +943,7 @@ func defaultGetMetricName(baseName string, _ pmetric.Metric) (string, error) {
 	return baseName, nil
 }
 
-// this function converts the OTEL spec metric to cloud monitoring.
+// this function converts the pdata metric to cloud monitoring.
 func (m *metricMapper) numberDataPointToValue(
 	point pmetric.NumberDataPoint,
 	metricKind metricpb.MetricDescriptor_MetricKind,
