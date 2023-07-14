b9e86fdd59b7ed6eadc8149955734dafad5cb40e
Mike Dame <mikedame@google.com>
2023-07-14 14:16:37 +0000

    Pass skip timestamp update to collector test

--- exporter/collector/integrationtest/metrics_test.go
+++ exporter/collector/integrationtest/metrics_test.go
@@ -42,7 +42,7 @@ func TestCollectorMetrics(t *testing.T) {
 		t.Run(test.Name, func(t *testing.T) {
 			test.SkipIfNeeded(t)
 
-			metrics := test.LoadOTLPMetricsInput(t, startTime, endTime, false)
+			metrics := test.LoadOTLPMetricsInput(t, startTime, endTime, test.SkipTimestampUpdate)
 
 			testServer, err := cloudmock.NewMetricTestServer()
 			require.NoError(t, err)
