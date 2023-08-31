e0f6140edaa9729401fc40b2dbb1bdf7a7e750ee
David Ashpole <dashpole@google.com>
2023-08-31 11:08:08 -0400

    Add an experimental googleclientauth extension (#703)
    * add gcp client auth extension

---
+++ extension/googleclientauthextension/config_test.go
@@ -0,0 +1,57 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package googleclientauthextension // import "github.com/GoogleCloudPlatform/opentelemetry-operations-go/extension/googleclientauthextension"
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap/confmaptest"
+
+	"github.com/GoogleCloudPlatform/opentelemetry-operations-go/extension/googleclientauthextension/internal/metadata"
+)
+
+func TestLoadConfig(t *testing.T) {
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
+	require.NoError(t, err)
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+
+	sub, err := cm.Sub(component.NewIDWithName(metadata.Type, "").String())
+	require.NoError(t, err)
+	require.NoError(t, component.UnmarshalConfig(sub, cfg))
+
+	assert.Equal(t, cfg.(*Config), factory.CreateDefaultConfig().(*Config))
+
+	sub, err = cm.Sub(component.NewIDWithName(metadata.Type, "customname").String())
+	require.NoError(t, err)
+	require.NoError(t, component.UnmarshalConfig(sub, cfg))
+
+	assert.Equal(t,
+		&Config{
+			Project:      "my-project",
+			Scopes:       []string{"https://www.something.com/hello", "https://www.something.com/world"},
+			QuotaProject: "other-project",
+		},
+		cfg,
+	)
+}
+
+func TestValidate(t *testing.T) {
+	assert.NoError(t, NewFactory().CreateDefaultConfig().(*Config).Validate())
+}
