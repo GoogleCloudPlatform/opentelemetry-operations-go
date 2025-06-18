214376d00c74923436243b766d25f46a652890e6
David Ashpole <dashpole@google.com>
2025-06-18 12:30:13 -0400

    prepare for v1.28.0/v0.53.0 (#1052)

--- tools/release.go
+++ tools/release.go
@@ -29,8 +29,8 @@ import (
 const (
 	prefix = "github.com/GoogleCloudPlatform/opentelemetry-operations-go"
 
-	stable   = "1.28.0"
-	unstable = "0.52.0"
+	stable   = "1.29.0"
+	unstable = "0.53.0"
 )
 
 var stableModules = map[string]struct{}{
