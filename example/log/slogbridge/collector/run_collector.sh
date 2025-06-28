535468611426bd08a17b006b772b72c4c77fc0b0
Pranav Sharma <sharmapranav@google.com>
2025-06-28 19:38:51 +0000

    Add OTel Logging sample

---
+++ example/log/slogbridge/collector/run_collector.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# A script to run the Google Cloud OTel Collector Docker image locally,
+# mounting a local YAML configuration file.
+#
+# Usage: ./run_collector.sh <path_to_your_config.yaml>
+#
+
+# --- Configuration ---
+# The full name of the Docker image to run.
+DOCKER_IMAGE="us-docker.pkg.dev/cloud-ops-agents-artifacts/google-cloud-opentelemetry-collector/otelcol-google:0.128.0"
+# The path inside the container where the config file will be mounted.
+CONFIG_FILE_CONTAINER="/tmp/otelcol-custom/config.yaml"
+
+# --- Script ---
+# Exit immediately if a command fails.
+set -e
+
+# Store the path to the host's config file from the first script argument.
+CONFIG_FILE_HOST="$1"
+
+# 1. Validate that a config file was provided.
+if [ -z "${CONFIG_FILE_HOST}" ]; then
+  echo "❌ Error: No config file supplied."
+  echo "Usage: $0 <path_to_your_config.yaml>"
+  exit 1
+fi
+
+# 2. Validate that the provided file exists.
+if [ ! -f "${CONFIG_FILE_HOST}" ]; then
+  echo "❌ Error: File not found at '${CONFIG_FILE_HOST}'"
+  exit 1
+fi
+
+# 3. Validate if the required environment variables were set.
+if [[ -z "${GOOGLE_CLOUD_PROJECT}" ]]; then
+  echo "❌ Error: GOOGLE_CLOUD_PROJECT environment variable not set"
+  exit 1
+fi
+
+if [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
+  echo "❌ Error: GOOGLE_APPLICATION_CREDENTIALS environment variable not set"
+  exit 1
+fi
+
+echo "🚀 Starting OpenTelemetry Collector..."
+echo "   Environment variables verified"
+echo "   Image: ${DOCKER_IMAGE}"
+echo "   Config File (Host): $(readlink -f "${CONFIG_FILE_HOST}")"
+echo "   Application Creds (Host): $(readlink -f "${GOOGLE_APPLICATION_CREDENTIALS}")"
+echo "   Google Project ID (Host): ${GOOGLE_CLOUD_PROJECT}"
+echo "   Press Ctrl+C to stop the collector."
+
+# 3. Run the Docker container.
+#    --rm      : Automatically remove the container when it exits.
+#    -it       : Run in interactive mode to see logs and allow Ctrl+C to stop it.
+#    -v        : Mount the local config file into the container.
+#    -e        : Set the required environment variables in the container.
+#    --config  : Pass the path of the mounted config file to the collector's command.
+docker run --rm -it \
+  --user "$(id -u)":"$(id -g)" \
+  -e "GOOGLE_CLOUD_PROJECT=${GOOGLE_CLOUD_PROJECT}" \
+  -e "GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS}" \
+  -v "$(readlink -f "${CONFIG_FILE_HOST}")":"${CONFIG_FILE_CONTAINER}":ro \
+  -v "${GOOGLE_APPLICATION_CREDENTIALS}:${GOOGLE_APPLICATION_CREDENTIALS}:ro" \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -p 4318:4318 \
+  "${DOCKER_IMAGE}" \
+  --config "${CONFIG_FILE_CONTAINER}"
---
+++ example/log/slogbridge/example.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"log/slog"
+	"runtime"
+
+	"go.opentelemetry.io/contrib/bridges/otelslog"
+	"go.opentelemetry.io/contrib/detectors/gcp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
+	"go.opentelemetry.io/otel/log/global"
+	otelsdklog "go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+)
+
+const (
+	name = "github.com/GoogleCloudPlatform/opentelemetry-operations-go/example/log/slogbridge/main"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Create OTLP Log exporter(s)
+	// OTLP log exporter to export structuted logs to an OTLP endpoint
+	otlpLogExporter, err := otlploghttp.New(ctx)
+	if err != nil {
+		log.Fatalf("failed to initialize OTLP log exporter")
+	}
+	// stdout log exporter to export structured logs to standard out
+	stdoutLogExporter, err := stdoutlog.New()
+	if err != nil {
+		log.Fatalf("failed to initialize stdout log exporter")
+	}
+
+	// Add resource attributes using GCP resource detector
+	res, err := resource.New(
+		ctx,
+		// Use the GCP resource detector to detect information about the GCP platform
+		resource.WithDetectors(gcp.NewDetector()),
+		// Keep the default detectors
+		resource.WithTelemetrySDK(),
+		// Add attributes from environment variables
+		resource.WithFromEnv(),
+		// Add your own custom attributes to identify your application
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("example-application"),
+		),
+	)
+	if errors.Is(err, resource.ErrPartialResource) || errors.Is(err, resource.ErrSchemaURLConflict) {
+		log.Println(err)
+	} else if err != nil {
+		log.Fatalf("resource.New: %v", err)
+	}
+
+	// setup OTel logger provider
+	// the logger provider is setup to export logs to both OTLP endpoint
+	// and standard out
+	loggerProvider := otelsdklog.NewLoggerProvider(
+		otelsdklog.WithProcessor(otelsdklog.NewBatchProcessor(otlpLogExporter)),
+		otelsdklog.WithProcessor(otelsdklog.NewBatchProcessor(stdoutLogExporter)),
+		otelsdklog.WithResource(res),
+	)
+
+	// Esnure provider shutdown to flush all logs before exit
+	defer func() {
+		if err = loggerProvider.Shutdown(ctx); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	// Set global logger provider
+	global.SetLoggerProvider(loggerProvider)
+
+	// emit structured logs to OTLP and standard out
+	logger := otelslog.NewLogger(name)
+	generateLogs(ctx, logger)
+
+}
+
+func generateLogs(ctx context.Context, logger *slog.Logger) {
+	logger.InfoContext(ctx, "Sample application log")
+	logger.InfoContext(ctx, "Sample log with key-value", "OS Name: ", runtime.GOOS)
+}
